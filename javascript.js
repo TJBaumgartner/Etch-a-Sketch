dd5fc50adf877fbb642da06b74665118c3e8caf0
TJBaumgartner <tnorth6@gmail.com>
2023-04-04 17:26:00 -0700

    Created hue button gradient color

--- javascript.js
+++ javascript.js
@@ -4,21 +4,33 @@ const buttonGrid = document.getElementById('buttonGrid');
 let mouseDown = false;
 const rainbowButton = document.getElementById('rainbowColor');
 const blackButton = document.getElementById('defaultColor');
+const hueButton = document.getElementById('hueColor');
 let rainbow = false;
 let black = true;
+let hue = false
+let hueScale = 0;
 
 rainbowButton.addEventListener('click', colorR);
 blackButton.addEventListener('click', colorB);
+hueButton.addEventListener('click', colorH);
+
 
 buttonGrid.addEventListener('click', gridInput);
 
 function colorR(){
     rainbow = true;
     black = false;
+    hue = false;
 }
 function colorB(){
     rainbow = false;
     black = true;
+    hue = false;
+}
+function colorH(){
+    rainbow = false;
+    black = false;
+    hue = true;
 }
 
 
@@ -64,7 +76,52 @@ function color (e) {
     else if(e.type === 'mouseover' && mouseDown == true && rainbow == true){
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
         this.style.backgroundColor = "#" + randomColor;
-    }    
+    }
+    else if(e.type === 'mouseover' && mouseDown == true && hue == true){
+        for(i = 0; i <=9; i++){
+            if(hueScale == 9){
+                this.style.backgroundColor = "#E2E2E2"; 
+                hueScale = 0;
+            }
+            if(hueScale == 8){
+                this.style.backgroundColor = "#C6C6C6";    
+                hueScale++;   
+                console.log(hueScale);
+            }
+            if(hueScale == 7){
+                this.style.backgroundColor = "#ABABAB";    
+                hueScale++;   
+            }
+            if(hueScale == 6){
+                this.style.backgroundColor = "#919191";
+                hueScale++;       
+            }
+            if(hueScale == 5){
+                this.style.backgroundColor = "#777777";    
+                hueScale++;   
+            }
+            if(hueScale == 4){
+                this.style.backgroundColor = "#5E5E5E";    
+                hueScale++;   
+            }
+            if(hueScale == 3){
+                this.style.backgroundColor = "#474747";  
+                hueScale++;     
+            }
+            if(hueScale == 2){
+                this.style.backgroundColor = "#303030";   
+                hueScale++;    
+            }
+            if(hueScale == 1){
+                this.style.backgroundColor = "#1B1B1B";  
+                hueScale++;     
+            }
+            if(hueScale == 0){
+                this.style.backgroundColor = "#000000";
+                hueScale++; 
+            }
+        }  
+    }
 }
 window.onload = () => {
     createGrid(numberOfDivs);
